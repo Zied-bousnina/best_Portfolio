44fd774d969d86abf9a1b3c16174249dbae382aa
Zied-bousnina <zbousnina@yahoo.com>
2023-06-10 20:32:37 +0100

    jg

--- src/Content.js
+++ src/Content.js
@@ -12,7 +12,7 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
+// import project1 from "./assets/images/projects/img1.png";
 import project2 from "./assets/images/projects/img2.png";
 import project3 from "./assets/images/projects/img3.png";
 import person_project from "./assets/images/projects/person.png";
@@ -139,7 +139,7 @@ export const content = {
     project_content: [
       {
         title: "Gym Website",
-        image: project1,
+        // image: project1,
       },
       {
         title: "Social Media web",
